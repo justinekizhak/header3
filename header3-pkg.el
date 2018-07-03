00557a30b159d1b461e5dfa83afe2461f15f5017
justine thomas <justinethomas009@gmail.com>
2018-07-03 22:35:13 +0530

    Add header3-pkg.el

---
+++ header3-pkg.el
@@ -0,0 +1,8 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "header3" "3.3" "Create files with awesome headers."
+  '(
+    (emacs "24.3")
+    (pkg-info "0.4")
+    (git-link)
+    (projectile)
+    ))
