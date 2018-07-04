812cd55ad5c9fa55c9c0e003b79095cedb04d5ba
justine thomas <justinethomas009@gmail.com>
2018-07-04 16:50:13 +0530

    Bump up version number

--- header3-pkg.el
+++ header3-pkg.el
@@ -1,5 +1,5 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "header3" "0.3" "Create files with awesome headers."
+(define-package "header3" "3.4.1" "Create files with awesome headers."
   '(
     (git-link "20180606.2139")
     (projectile "20180621.2239")
