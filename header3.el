3fc5782cd35ceeda970623c83f2843bd6e84a2ac
justine thomas <justinethomas009@gmail.com>
2018-07-11 00:29:19 +0530

    Update header-maintainer function.

--- header3.el
+++ header3.el
@@ -13,9 +13,9 @@
 ;; Created: Tue Aug  4 17:06:46 1987
 ;; Version: 3.4.2
 ;; Package-Requires: ((projectile "0.14.0") (git-link "0.7.0"))
-;; Last-Updated: Tue 10 Jul 2018 21:45:43 IST
+;; Last-Updated: Wed 11 Jul 2018 00:28:46 IST
 ;;           By: Justine T Kizhakkinedath
-;;     Update #: 2157
+;;     Update #: 2158
 ;; URL: https://github.com/justinethomas009/header3
 ;; Doc URL: https://emacswiki.org/emacs/AutomaticFileHeaders
 ;; Keywords: tools, docs, maint, abbrev, local
@@ -837,7 +837,9 @@ Launches the \"insert-file\" function after comparing with the license name"
 (defsubst header-maintainer ()
   "Insert \"Maintainer: \" line."
   (insert header-prefix-string "Maintainer: ")
-  (insert (user-full-name) "\n"))
+  ;; (insert (user-full-name) "\n")
+  (insert
+   (shell-command-to-string "git log --format='%an <%ae>' -1")))
 
 (defun header-copyright ()
   "Insert `header-copyright-notice', unless nil."
@@ -1137,6 +1139,7 @@ It is sensitive to language-dependent comment conventions."
      comment-start)
     (t ";; ")))       ; Use Lisp as default.
 
+;; For autoloading functions
 ;;;###autoload
 (autoload 'auto-make-header "header3")
 
